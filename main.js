c0436345498bba02b7edb1eb0e525a37ac9d551b
colombelli <xcolombelli@gmail.com>
2018-10-27 17:07:10 -0200

    Selectors of measurement (bytes/kbytes/...) and introduction of OOP

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-
-
-
-
 // Function that converts a decimal number to a binary string
 function dec2bin(dec) {
     return (dec >>> 0).toString(2);
 }
+
+// Function that converts a binary string to a decimal number
+function bin2dec(bin) {
+	return parseInt(bin, 2);
+}
+
+// constructor for cache object
+function Cache(cacheSize){
+	this.size = cacheSize;  // numeric int indicating the size of the cache memory
+	this.measure = document.getElementById('cacheSizeMeasure').value;  // string indicating if the size is in bytes, kb or mb
+	this.sizeInBytes =   // the cache size converted to a integer byte value (in base-2)
+		function(){ 
+			if (this.measure == "cacheBytes")
+				return cacheSize;
+			else if (this.measure == "cacheKbytes")
+				return this.size * 1024;
+			else
+				return this.size * 1048576;
+		}
+	this.binStr = dec2bin(this.sizeInBytes());
+}
+
 
 
 // Function that calculates the mapping
@@ -29,7 +47,7 @@ function calculateMapping(cacheSize, blockSize, address) {
   //var wordBinaryString = binaryAddress.substr(bitsTag+bitsBlock);
   var blockBinaryString = binaryAddress.substr(bitsTag, bitsBlock);
 
-  // final block
+  // final block - transforming the binary string back to numeric integer 
   var finalBlockValue = parseInt(blockBinaryString, 2);
 
   blockAddressHTML.innerHTML = "Endereço por mapeamento direto - bloco: " + finalBlockValue;
@@ -46,8 +64,12 @@ function calculateMapping(cacheSize, blockSize, address) {
 function getValues() {
 
   var cacheSizeString = document.getElementById('cacheSize');
-  if (cacheSizeString.value) // check if there is, indeed, a input value
+  if (cacheSizeString.value){ // check if there is, indeed, a input value
     var cacheSize = parseInt(cacheSizeString.value); // numeric int value of cache size input
+	var cache = new Cache(cacheSize);
+	console.log(cache.sizeInBytes());
+	console.log(cache.binStr);
+  }
   else {
     alert('Você precisa passar um valor para o tamanho da memória cache!');
     return false;
@@ -55,8 +77,10 @@ function getValues() {
 
 
   var blockSizeString = document.getElementById('blockSize');
-  if (blockSizeString.value) // check if there is, indeed, a input value
+  if (blockSizeString.value){ // check if there is, indeed, a input value
     var blockSize = parseInt(blockSizeString.value); // numeric int value of block size input
+	
+  }
   else {
     alert('Você precisa passar um valor para o tamanho dos blocos!');
     return false;
