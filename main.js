fd9f0af5d1711b332085ee05ee9b96593bf89aa9
colombelli <xcolombelli@gmail.com>
2019-01-01 18:44:13 -0200

    Group associative mapping working

--- main.js
+++ main.js
@@ -49,24 +49,39 @@ function calculateMapping(cache, block, address) {
   var numBlocksHTML = document.getElementById('numBlocks');
   numBlocksHTML.innerHTML = "Número de blocos da cache: " + numBlocks; // shows result on numBlocks paragraph
 
+
   var binAddress = dec2bin(address);  // Converts the integer of the addres to a binary string
   var bitsBlock = Math.log2(numBlocks); // Calculates how many bits will give the block
   var bitsWord = Math.log2(block.sizeInBytes()); // Calculates how many bits will give the word
   var bitsTag = binAddress.length - (bitsWord + bitsBlock); // Calculates how many bits will give the tag
 
+  // get the integer value of the group associative map, e.g. 2-way equals 2
+  var setAssociativeMap = parseInt(document.getElementById('groupAssociative').value)
 
+  // Calculates how many bits will give the set and the tag considering a group associative mapping
+  bitsSet = Math.log2(cache.sizeInBytes() / (block.sizeInBytes() * setAssociativeMap))
+  bitsTagS = binAddress.length - (bitsWord + bitsSet);
 
+  console.log(bitsSet)
   // Calculates block address
 
   var blockAddressHTML = document.getElementById('directMapping');
+  var setAddressHTML = document.getElementById('groupAssociativeMapping')
 
   // catches the substring corresponding to what matters
   var wordBinaryString = binAddress.substr(bitsTag+bitsBlock); // withouth comma: from that till the end
   var blockBinaryString = binAddress.substr(bitsTag, bitsBlock);
+  var setBinaryString = binAddress.substr(bitsTagS, bitsSet);
   // for the tag, we first need to check if it is 0
   var tagBinaryString = 0;
   if (bitsTag != 0)
     var tagBinaryString = binAddress.substr(0, bitsBlock);
+  // repeating the same process to bitsTagS
+  var tagSBinaryString = 0;
+  if (bitsTagS != 0)
+    var tagSBinaryString = binAddress.substr(0, bitsSet);
+
+
 
 
   // final block - transforming the binary string back to numeric integer
@@ -75,15 +90,20 @@ function calculateMapping(cache, block, address) {
       return parseInt(wordBinaryString, 2)},
     block: function(){
       return parseInt(blockBinaryString, 2)},
+    set: function(){
+      return parseInt(setBinaryString, 2) * setAssociativeMap},
+    fBlockSet: function(){
+      return setAssociativeMap - 1 + this.set()},
     tag: function(){
       return parseInt(tagBinaryString, 2)},
-    stringToConcatenate: function(){
-      return this.block() + " | palavra " + this.word() + " | tag " + this.tag()}
+    stringToConcatenate1: function(){
+      return this.block() + " | palavra " + this.word() + " | tag " + this.tag()},
+    stringToConcatenate2: function(){
+      return "do bloco " + this.set() + " ao bloco " + this.fBlockSet()}
   }
 
-  blockAddressHTML.innerHTML = "Endereço por mapeamento direto: bloco " + finalValues.stringToConcatenate();
-
-
+  blockAddressHTML.innerHTML = "Endereço por mapeamento direto: bloco " + finalValues.stringToConcatenate1();
+  setAddressHTML.innerHTML = "Endereço por mapeamento grupo associativo: " + finalValues.stringToConcatenate2();
 
 
   return false;
